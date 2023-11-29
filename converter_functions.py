30eacb14c64533bf4edf23abe3e59790a413e6f3
Thomas Griffin Jayasankar <tjayasa@gl-login3.arc-ts.umich.edu>
2023-11-29 11:07:43 -0500

    Added functions to check for plane-plane plane-line, and line-line intersection

--- converter_functions.py
+++ converter_functions.py
@@ -21,7 +21,7 @@ def find_intersection_line(plane_dict1, plane_dict2):
     normal_vector2 = normal_vector2/np.linalg.norm(normal_vector2)
 
     #Check that the two planes are not parallel (i.e. non-intersecting):
-    if np.allclose(normal_vector1, normal_vector2):
+    if not do_planes_intersect(plane_dict1, plane_dict2):
         print("Planes Do Not Intersect!")
         return [0, 0, 0, 0, 0, 0]
 
@@ -89,3 +89,163 @@ def find_intersection_line(plane_dict1, plane_dict2):
     print(f"Point: ({x}, {y}, {z}), Direction: <{direction_vector[0]}, {direction_vector[1]}, {direction_vector[2]},>")
 
     return [x, y, z, direction_vector[0], direction_vector[1], direction_vector[2]]
+
+
+def do_planes_intersect(plane_dict1, plane_dict2):
+    #Parametric Form of 3D Line:
+    # x = x0 + alpha*t
+    # y = y0 + beta*t
+    # z = z0 + epsilon*t
+
+    #Equation of a Plane:
+    # Ax + By + Cz = D
+
+    a1 = plane_dict1["a"]
+    b1 = plane_dict1["b"]
+    c1 = plane_dict1["c"]
+
+    a2 = plane_dict2["a"]
+    b2 = plane_dict2["b"]
+    c2 = plane_dict2["c"]
+
+    normal_vector1 = np.array([a1, b1, c1])
+    normal_vector2 = np.array([a2, b2, c2])
+
+    #Normalizing the normal vectors:
+    normal_vector1 = normal_vector1/np.linalg.norm(normal_vector1)
+    normal_vector2 = normal_vector2/np.linalg.norm(normal_vector2)
+
+    #tolerance = 1e-5
+    #print(np.dot(np.array([a, b, c]), np.array([alpha, beta, epsilon])))
+
+    return (not np.allclose(normal_vector1, normal_vector2))
+
+def find_plane_line_intersection_point(plane_dict, line_dict):
+    #Parametric Form of 3D Line:
+    # x = x0 + alpha*t
+    # y = y0 + beta*t
+    # z = z0 + epsilon*t
+
+    #Equation of a Plane:
+    # Ax + By + Cz = D
+
+    x0 = line_dict["x0"]
+    y0 = line_dict["y0"]
+    z0 = line_dict["z0"]
+    alpha = line_dict["alpha"]
+    beta = line_dict["beta"]
+    epsilon = line_dict["epsilon"]
+
+    a = plane_dict["a"]
+    b = plane_dict["b"]
+    c = plane_dict["c"]
+    d = plane_dict["d"]
+
+    if not does_line_intersect_plane(plane_dict, line_dict):
+        raise ValueError("Requested Line and Plane do not Intersect")
+
+    t = (d - (a*x0) - (b*y0) - (c*z0))/((alpha*a) + (beta*b) + (epsilon*c))
+
+    x = x0 + (alpha*t)
+    y = y0 + (beta*t)
+    z = z0 + (epsilon*t)
+
+    print(f"Point ({x}, {y}, {z})")
+
+def does_line_intersect_plane(plane_dict, line_dict):
+    #Parametric Form of 3D Line:
+    # x = x0 + alpha*t
+    # y = y0 + beta*t
+    # z = z0 + epsilon*t
+
+    #Equation of a Plane:
+    # Ax + By + Cz = D
+
+    alpha = line_dict["alpha"]
+    beta = line_dict["beta"]
+    epsilon = line_dict["epsilon"]
+
+    a = plane_dict["a"]
+    b = plane_dict["b"]
+    c = plane_dict["c"]
+
+    tolerance = 1e-5
+
+    #print(np.dot(np.array([a, b, c]), np.array([alpha, beta, epsilon])))
+
+    return(abs(np.dot(np.array([a, b, c]), np.array([alpha, beta, epsilon]))) > tolerance)
+
+def find_line_line_intersection_point(line_dict1, line_dict2):
+    #Parametric Form of 3D Line:
+    # x = x0 + alpha*t
+    # y = y0 + beta*t
+    # z = z0 + epsilon*t
+
+    tolerance = 1e-5
+
+    x01 = line_dict1["x0"]
+    y01 = line_dict1["y0"]
+    z01 = line_dict1["z0"]
+    alpha1 = line_dict1["alpha"]
+    beta1 = line_dict1["beta"]
+    epsilon1 = line_dict1["epsilon"]
+
+    x02 = line_dict2["x0"]
+    y02 = line_dict2["y0"]
+    z02 = line_dict2["z0"]
+    alpha2 = line_dict2["alpha"]
+    beta2 = line_dict2["beta"]
+    epsilon2 = line_dict2["epsilon"]
+
+    p1 = np.array([x01, y01, z01])
+    p2 = np.array([x02, y02, z02])
+
+    v1 = np.array([alpha1, beta1, epsilon1])
+    v2 = np.array([alpha2, beta2, epsilon2])
+
+    a = np.array([[alpha1, -1*alpha2], [beta1, -1*beta2], [epsilon1, -1*epsilon2]])
+    b = np.array([[x02 - x01], [y02 - y01], [z02 - z01]])
+
+    t = np.linalg.lstsq(a,b,rcond=None)[0][0]
+    print(f"t = {t}")
+
+    x = x01 + alpha1*t
+    y = y01 + beta1*t
+    z = z01 + epsilon1*t
+
+    print(f"Intersection point = ({x}, {y}, {z})")
+
+
+
+def do_lines_intersect(line_dict1, line_dict2):
+    #Parametric Form of 3D Line:
+    # x = x0 + alpha*t
+    # y = y0 + beta*t
+    # z = z0 + epsilon*t
+
+    #Vector form of a line:
+    # Line = p + (Real Number)*v
+
+    tolerance = 1e-5
+
+    x01 = line_dict1["x0"]
+    y01 = line_dict1["y0"]
+    z01 = line_dict1["z0"]
+    alpha1 = line_dict1["alpha"]
+    beta1 = line_dict1["beta"]
+    epsilon1 = line_dict1["epsilon"]
+
+    x02 = line_dict2["x0"]
+    y02 = line_dict2["y0"]
+    z02 = line_dict2["z0"]
+    alpha2 = line_dict2["alpha"]
+    beta2 = line_dict2["beta"]
+    epsilon2 = line_dict2["epsilon"]
+
+    p1 = np.array([x01, y01, z01])
+    p2 = np.array([x02, y02, z02])
+
+    v1 = np.array([alpha1, beta1, epsilon1])
+    v2 = np.array([alpha2, beta2, epsilon2])
+
+    return (abs(np.dot(np.cross(v1, v2), (p1-p2))) < tolerance)
